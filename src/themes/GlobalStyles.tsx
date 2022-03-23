10261fc4ad643d69480a53b2a87deb555a9a3293
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-03-23 12:50:41 +0100

    Create theme folder for global styles and theme

---
+++ src/themes/GlobalStyles.tsx
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+@font-face {
+    font-family:'Neurial Grotesk';
+    src:url(${"../assets/fonts/NeurialGrotesk/NeurialGrotesk-Regular.otf"})
+}
+*, *::before, *::after{
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    
+}
+html{
+    font-size: 62.5%;
+    font-family:'Neurial Grotesk',sans-serif;
+    margin: 0;
+    padding: 0;
+    scroll-behavior: smooth;
+    width: 100vw; 
+}
+ body{
+  margin: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  letter-spacing: -0.03em;
+  min-height: 100vh;
+  font-size: 1.6rem;
+  color: black;
+}
+p,h1, h2, h3,ul ,li,a{
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+}
+`;
+export default GlobalStyles;
---
+++ src/themes/MainTheme.tsx
@@ -0,0 +1,14 @@
+export const theme = {
+	colors: {
+		actionPrimaryGreen: "hsla(156, 48%, 56%, 1)",
+		actionSecondaryPurple: "hsla(196, 100%, 17%, 1)",
+		darkPurpleSectionBg: "hsla(240, 30%, 28%, 1)",
+		grey1pxDividingLine: "hsla(213, 28%, 92%, 1)",
+		inputBg: "hsla(213, 28%, 92%, 1)",
+		lightGreySectionBg: "hsla(213, 28%, 92%, 1)",
+		textDarkGreyIsh: "hsla(213, 28%, 92%, 1)",
+		textDarkNavy: "hsla(207, 59%, 17%, 1)",
+		text: "hsla(0, 0%, 71%, 1)",
+		gold: "hsla(41, 55%, 56%, 1)",
+	},
+};

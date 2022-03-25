bfe55a6d6a72c85122670ded89a7d0281463ee53
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-03-25 09:14:57 +0100

    Format GlobalStyle and temporary change font family
    Importing font Neurial  from assets to GlobalStyles breaks the app.
    Issue needs fix later on. Temporary set other default font.

--- src/themes/GlobalStyles.tsx
+++ src/themes/GlobalStyles.tsx
@@ -1,39 +1,45 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-@font-face {
+/* @font-face {
     font-family:'Neurial Grotesk';
     src:url(${"../assets/fonts/NeurialGrotesk/NeurialGrotesk-Regular.otf"})
-}
-*, *::before, *::after{
+} */
+*,
+  *::before,
+  *::after {
     box-sizing: inherit;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    
-}
-html{
-  
+  }
+  html {
     font-size: 62.5%;
-    font-family:'Neurial Grotesk',sans-serif;
+    font-family: Arial, Helvetica, sans-serif;
     margin: 0;
     padding: 0;
     scroll-behavior: smooth;
-    width: 100vw; 
-}
- body{
-  box-sizing: border-box;
-  margin: 0;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  letter-spacing: -0.03em;
-  min-height: 100vh;
-  font-size: 1.6rem;
-  color: black;
-}
-p,h1, h2, h3,ul ,li,a{
+    width: 100vw;
+  }
+  body {
+    box-sizing: border-box;
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    letter-spacing: -0.03em;
+    min-height: 100vh;
+    font-size: 1.6rem;
+    color: black;
+  }
+  p,
+  h1,
+  h2,
+  h3,
+  ul,
+  li,
+  a {
     margin: 0;
     padding: 0;
     text-decoration: none;
-}
+  }
 `;
 export default GlobalStyles;

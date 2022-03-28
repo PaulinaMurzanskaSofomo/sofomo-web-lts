255b17eb9e5409d198c3f5d0e91dc490edad3bf7
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-03-28 12:50:45 +0200

    Add  asset:about, add new items to developers
    logotypes added, to add it later to map function,also added new content
    needed to menu

--- src/assets/content/developers.ts
+++ src/assets/content/developers.ts
@@ -1,3 +1,10 @@
+import backend from "../icons/outsourcepageIcons/Backend.svg";
+import frontend from "../icons/outsourcepageIcons/Frontendi.svg";
+import mobile from "../icons/outsourcepageIcons/Mobile.svg";
+import magento from "../icons/outsourcepageIcons/Magento.svg";
+import salesforce from "../icons/outsourcepageIcons/Salesforce.svg";
+import cloud from "../icons/outsourcepageIcons/AWSCloud.svg";
+
 export const developers = [
   {
     type: "backend developers",
@@ -11,30 +18,42 @@ export const developers = [
       "golang developers",
     ],
     description: `We support all major backend technologies.`,
+    icon: `${backend}`,
+    id: 1,
   },
   {
     type: "frontend developers",
     role: ["react developers", "angular developers", "vue.Js developers"],
     description: `We're specialists in React, Angular & Vue.js.`,
+    icon: `${frontend}`,
+    id: 2,
   },
   {
     type: "mobile developers",
     role: ["IOS developers", "android developers", "react native delevopers"],
     description: `Launching a new app ? Lets's do it! iOS, Android & React Native.`,
+    icon: `${mobile}`,
+    id: 3,
   },
   {
-    type: "Cloud/AWS",
-    role: "AWS Engineers",
+    type: "AWS Cloud Engineers",
+    role: ["AWS Engineers"],
     description: `We've done it all from basic to advanced AWS infranstructure.`,
+    icon: `${cloud}`,
+    id: 4,
   },
   {
-    type: "Salesforce",
-    role: "Salesforce Developers",
+    type: "Salesforce developers",
+    role: ["Salesforce Developers"],
     description: `Take advantage of Salesforce, we've got you covered.`,
+    icon: `${salesforce}`,
+    id: 5,
   },
   {
-    type: "Microsoft Stack",
-    role: ".Net Developers",
+    type: "Magento",
+    role: [".Net Developers"],
     description: `We're experts in .Net framework/Core.`,
+    icon: `${magento}`,
+    id: 6,
   },
 ];
---
+++ src/assets/content/navMenu/about.ts
@@ -0,0 +1,2 @@
+export const whatWeDo = `Trusted developers, ready to join your team.`;
+export const advantages = `Lorem ipsum dolor sit amet, consecretur adipiscing elit`;

2c1735f8e87b7b4930c1a98b1f9e7c4b83644315
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-03-29 09:37:14 +0200

    Add path key to mainMenu array
    This path is needed to implement Navlink(reat-router-dom), so it could
    automatically add and remove active class, based on currently clicked item

--- src/assets/content/navMenu/mainMenu.ts
+++ src/assets/content/navMenu/mainMenu.ts
@@ -1,7 +1,7 @@
 export const mainMenu = [
-  { title: "what we do", chevron: true, id: 1 },
-  { title: "our developers", chevron: true, id: 2 },
-  { title: "why sofomo ?", chevron: false, id: 3 },
-  { title: "clients", chevron: false, id: 4 },
-  { title: "about us", chevron: true, id: 5 },
+  { title: "what we do", chevron: true, id: 1, path: "/what_we_do" },
+  { title: "our developers", chevron: true, id: 2, path: "developers" },
+  { title: "why sofomo ?", chevron: false, id: 3, path: "why_sofomo" },
+  { title: "clients", chevron: false, id: 4, path: "clients" },
+  { title: "about us", chevron: true, id: 5, path: "about_us" },
 ];

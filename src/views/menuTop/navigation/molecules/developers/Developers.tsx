d72da26a28a8aaad68c1bc96408090583475954e
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-03-30 11:09:27 +0200

    Change structure of Developers +styles restructure

--- src/views/menuTop/navigation/molecules/developers/Developers.tsx
+++ src/views/menuTop/navigation/molecules/developers/Developers.tsx
@@ -1,22 +1,30 @@
 import React, { FC } from "react";
 import { developers } from "../../../../../assets/content/developers";
 import DeveloperBlock from "./DeveloperBlock";
-import { StyledContentWrapper } from "../../templates/StyledContentSwitch";
+import ImageBox from "../../atoms/ImageBox";
+import { StyledDevelopersWrapper } from "./StyledDevelopers";
 
 const Developers: FC = () => {
   return (
-    <StyledContentWrapper className="developers">
+    <StyledDevelopersWrapper className="developers">
       <h3>hire developers</h3>
-      {developers.map((dev) => (
-        <DeveloperBlock
-          title={dev.type}
-          icon={dev.icon}
-          description={dev.description}
-          roles={dev.role}
-          key={dev.id}
-        />
-      ))}
-    </StyledContentWrapper>
+      <div className="roles-image-wrapper">
+        <div className="roles-wrapper">
+          {developers.map((dev) => (
+            <DeveloperBlock
+              title={dev.type}
+              icon={dev.icon}
+              description={dev.description}
+              roles={dev.role}
+              key={dev.id}
+            />
+          ))}
+        </div>
+        <div className="image-box">
+          <ImageBox />
+        </div>
+      </div>
+    </StyledDevelopersWrapper>
   );
 };
 
--- src/views/menuTop/navigation/molecules/developers/StyledDevelopers.tsx
+++ src/views/menuTop/navigation/molecules/developers/StyledDevelopers.tsx
@@ -1,80 +1,69 @@
 import styled from "styled-components";
 import { theme } from "../../../../../themes/MainTheme";
 
-export const StyledRoles = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  justify-content: flex-start;
+export const StyledDevelopersWrapper = styled.div`
+  box-sizing: border-box;
   position: relative;
-  padding: 1.5rem;
-  img {
+  flex-direction: column;
+  width: calc(100% + 3rem);
+  margin-left: -1.5rem;
+  padding-top: 1.5rem;
+  h3 {
     position: absolute;
-    top: 1.5rem;
-    left: 1.5rem;
-    width: 1.8rem;
-    height: 1.8rem;
-  }
-  &:hover {
-    background-color: ${theme.colors.hoverBgr};
-    transition: 0.3s;
+    top: 0;
+    left: 3rem;
+    text-transform: uppercase;
+    font-size: 1.2rem;
+    color: ${theme.colors.paragraph};
+    font-weight: 400;
+    line-height: 2rem;
+    display: none;
   }
   @media (min-width: 768px) {
-    width: 30%;
-    margin-right: 0.5rem;
-  }
-`;
-
-export const StyledDevDisplay = styled.div`
-  width: 100%;
-  padding-left: 3rem;
-  &:hover {
-    h4 {
-      color: ${theme.colors.activeHeaders};
+    padding-top: 4.2rem;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin-top: 1.5rem;
+    h3 {
+      display: block;
+      left: 1.5rem;
     }
   }
-`;
-
-export const StyledDescription = styled.div`
-  display: none;
   @media (min-width: 1094px) {
-    display: block;
-    &.clicked-dev-open {
+    width: 1094px;
+    margin: 4rem auto 0;
+    padding: 4.2rem 1.5rem;
+
+    h3 {
+      left: 3rem;
+    }
+  }
+  .roles-image-wrapper {
+    display: flex;
+    flex-wrap: nowrap;
+    width: 100%;
+    height: auto;
+    .roles-wrapper {
+      width: 100%;
+      display: flex;
+      position: relative;
+      flex-direction: column;
+      @media (min-width: 768px) {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        width: 100%;
+      }
+      @media (min-width: 1094px) {
+        width: calc(100% - 24rem);
+      }
+    }
+    .image-box {
       display: none;
-    }
-  }
-`;
-
-export const StyledRoleList = styled.ul`
-  margin-top: 10px;
-  @media (min-width: 1094px) {
-    display: none;
-    &.clicked-dev-open {
-      display: block;
-    }
-  }
-`;
-
-export const StyledRoleItem = styled.li`
-  width: 100%;
-  list-style-type: none;
-  text-transform: capitalize;
-  display: flex;
-  align-items: center;
-
-  display: flex;
-  align-items: center;
-  svg {
-    display: none;
-    color: ${theme.colors.activeHeaders};
-  }
-  &:hover {
-    p {
-      color: ${theme.colors.activeHeaders};
-      transform: scale(1.02);
-    }
-    svg {
-      display: block;
+      @media (min-width: 1094px) {
+        display: block;
+      }
     }
   }
 `;

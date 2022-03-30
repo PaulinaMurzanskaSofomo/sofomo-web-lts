51ee6de5bdba6b7f702762196d2df87211a7fe1b
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-03-30 18:12:08 +0200

    Add logic for nav clicks full and mobile views
    Created function that checks if menu is dropped and active, and based on
    that
    chevrons change direction and color, menu opens and closes, click on the
    next link closes the previous one.

--- src/views/menuTop/navigation/molecules/navItem/NavItem.tsx
+++ src/views/menuTop/navigation/molecules/navItem/NavItem.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useRef, useState } from "react";
 import { StyledItem } from "./StyledNavItem";
-
 import { NavLink } from "react-router-dom";
 import NavHeader from "./NavHeader";
 import ContentSwitch from "../../templates/ContentSwitchMobile";
+import { useSelectedNavlink } from "../../../../../globalState/zustandStores/useSelectedNavlink";
+import { useFullScreenMenuOpenStore } from "../../../../../globalState/zustandStores/useMenuOpen";
 
 interface Props {
   item: string;
@@ -13,9 +14,27 @@ interface Props {
 }
 
 const NavItem: FC<Props> = ({ item, id, path, chevron }) => {
+  const setId = useSelectedNavlink((state: any) => state.setSelectedItemId);
+  const isMenuDropped = useFullScreenMenuOpenStore((state: any) => state.toggleFullScreenMenu);
+
+  const handleMenuDropdown = (e: any) => {
+    const target = e.currentTarget;
+    setId(target.value);
+    const displayEl = document.querySelector(".display");
+    const navlinkEl = document.querySelector(`.navlink${id}`);
+
+    if (displayEl?.classList.contains("menu-dropped") && navlinkEl?.classList.contains("active")) {
+      isMenuDropped(false);
+      navlinkEl?.classList.remove("active");
+    } else {
+      isMenuDropped(true);
+      navlinkEl?.classList.add("active");
+    }
+  };
+
   return (
-    <StyledItem>
-      <NavLink to={path ? path : "/"}>
+    <StyledItem onClick={handleMenuDropdown} value={id}>
+      <NavLink to={path ? path : "/"} className={`navlink${id}`}>
         <NavHeader item={item} chevron={chevron} />
         <ContentSwitch id={id} />
       </NavLink>

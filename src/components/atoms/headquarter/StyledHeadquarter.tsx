1242aa5dd3dce6ce14dce0e911aab20d2f31d2fa
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-04-02 08:03:30 +0200

    Add StyledHeadquarter as atoms

---
+++ src/components/atoms/headquarter/StyledHeadquarter.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { theme } from "../../../themes/MainTheme";
+
+export const StyledHeadquarter = styled.div`
+  width: 12.6rem;
+  padding: 0.3rem 2rem;
+  border-radius: 1.3rem;
+  background: ${theme.colors.hoverBgr};
+  color: ${theme.colors.backgroudBlue};
+  border: 1px solid ${theme.colors.backgroudBlue};
+  font-size: 1.2rem;
+  font-weight: 700;
+`;
---
+++ src/components/atoms/headquarter/index.ts
@@ -0,0 +1,1 @@
+export { StyledHeadquarter } from "./StyledHeadquarter";

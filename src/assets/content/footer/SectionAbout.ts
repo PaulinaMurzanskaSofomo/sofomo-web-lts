df72cb9ef8f8ac46a86d7af3dec124cc440e5e6f
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-04-02 07:55:37 +0200

    Correct tabs for spaces
    Also add new content to developers.ts

--- src/assets/content/footer/SectionAbout.ts
+++ src/assets/content/footer/SectionAbout.ts
@@ -1,31 +1,26 @@
 export const about = `About us`;
 export const aboutText = `We build & support your own talented, trusted, full-time development team hosted out of Sofomo’s headquarters in Poland.`;
+export const sectionBlueHeader = `Companies big and small alike choose Sofomo as their development partner`;
 export const aboutList = [
-	`What We Do`,
-	`Our Advantage`,
-	`Clients`,
-	`About Us`,
-	`Why Poland`,
-	`FAQ’s`,
-	`Careers`,
-	`Get Started`,
-	`Contact Us`,
+  `What We Do`,
+  `Our Advantage`,
+  `Clients`,
+  `About Us`,
+  `Why Poland`,
+  `FAQ’s`,
+  `Careers`,
+  `Get Started`,
+  `Contact Us`,
 ];
-export const developers = [
-	`React`,
-	`Angular`,
-	`PHP`,
-	`Ruby`,
-	`.Net`,
-	`Node.js`,
-	`Golang`,
-	`Magento`,
-	`Java`,
-	`Python`,
-	`iOS`,
-	`Android`,
-	`Salesforce`,
-	`AWS`,
-	`Azure`,
+export const developers1 = [
+  `React`,
+  `Angular`,
+  `PHP`,
+  `Ruby`,
+  `.Net`,
+  `Node.js`,
+  `Golang`,
+  `Magento`,
 ];
+export const developers2 = [`Java`, `Python`, `iOS`, `Android`, `Salesforce`, `AWS`, `Azure`];
 export const copyrights = `© All rights reserved Sofomo 2020`;

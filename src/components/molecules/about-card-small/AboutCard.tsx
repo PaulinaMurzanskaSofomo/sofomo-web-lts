290a65d946c41196509f0fd59a0c2ac7f4a8e360
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-04-02 08:31:56 +0200

    Add AboutCard as molecules
    This is a component used in footer last section with socials icons and
    some text

---
+++ src/components/molecules/about-card-small/AboutCard.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from "react";
+import { StyledAboutCard } from "./StyledAboutCard";
+import { about, aboutText } from "../../../assets/content/footer/SectionAbout";
+import { StyledHeader } from "../../atoms/headers";
+import { StyledParagraph } from "../../atoms/paragraph";
+import { theme } from "../../../themes/MainTheme";
+import { Socials } from "../../atoms/socials";
+
+const AboutCard: FC = () => {
+  return (
+    <StyledAboutCard>
+      <StyledHeader transform="capitalize" fontWeight="700" className="header">
+        {about}
+      </StyledHeader>
+      <div className="text-block">
+        <StyledParagraph fontSize="1.4rem" color={theme.colors.fontLightGrey}>
+          {aboutText}
+        </StyledParagraph>
+      </div>
+      <Socials />
+    </StyledAboutCard>
+  );
+};
+
+export default AboutCard;
---
+++ src/components/molecules/about-card-small/index.ts
@@ -0,0 +1,2 @@
+export { default as AboutCard } from "./AboutCard";
+export { StyledAboutCard } from "./StyledAboutCard";

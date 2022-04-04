34c73a633f6302805de84ad3865430bfec728725
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-04-04 19:57:54 +0200

    Move organisms back to componentsq

--- src/components/organisms/footer/footer_main_view/StyledFooter.tsx
+++ src/components/organisms/footer/footer_main_view/StyledFooter.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { theme } from "../../../themes/MainTheme";
+import { theme } from "../../../../themes/MainTheme";
 
 export const StyledFooterWrapper = styled.footer`
   margin-top: 8rem;

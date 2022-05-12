4fa3e29119aaa7e91c96e01c04d149da29967862
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-05-12 14:48:32 +0200

    Create ListItem and add needed there icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+import arrowRightDotted from "./ArrowRight.svg";
+import arrowRightPlain from "./ArrowRightPlain.svg";
+import minus from "./Minus.svg";
+import plus from "./Plus.svg";
+export { arrowRightDotted, arrowRightPlain, minus, plus };

147ed814054650754f8b79ee22e925c51ab48284
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-05-12 13:13:25 +0200

    Change content data and add routes

--- src/assets/content/footer/SectionAbout.ts
+++ src/assets/content/footer/SectionAbout.ts
@@ -1,31 +1,114 @@
 export const about = `About us`;
 export const aboutText = `We build & support your own talented, trusted, full-time development team hosted out of Sofomo’s headquarters in Poland.`;
+export const sectionBlueHeader = `Companies big and small alike choose Sofomo as their development partner`;
+import { OUTSOURCE } from "../../../routes";
 export const aboutList = [
-	`What We Do`,
-	`Our Advantage`,
-	`Clients`,
-	`About Us`,
-	`Why Poland`,
-	`FAQ’s`,
-	`Careers`,
-	`Get Started`,
-	`Contact Us`,
+  ["Sofomo"],
+  [
+    {
+      title: `What We Do`,
+      path: OUTSOURCE,
+    },
+    {
+      title: `Our Advantage`,
+      path: OUTSOURCE,
+    },
+    {
+      title: `Clients`,
+      path: OUTSOURCE,
+    },
+    {
+      title: `About Us`,
+      path: OUTSOURCE,
+    },
+    {
+      title: `Why Poland`,
+      path: OUTSOURCE,
+    },
+    {
+      title: `FAQ’s`,
+      path: OUTSOURCE,
+    },
+    {
+      title: `Careers`,
+      path: OUTSOURCE,
+    },
+    {
+      title: `Get Started`,
+      path: OUTSOURCE,
+    },
+    {
+      title: `Contact Us`,
+      path: "",
+    },
+  ],
 ];
-export const developers = [
-	`React`,
-	`Angular`,
-	`PHP`,
-	`Ruby`,
-	`.Net`,
-	`Node.js`,
-	`Golang`,
-	`Magento`,
-	`Java`,
-	`Python`,
-	`iOS`,
-	`Android`,
-	`Salesforce`,
-	`AWS`,
-	`Azure`,
+export const developers1 = [
+  {
+    title: `React`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `Angular`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `PHP`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `Ruby`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `.Net`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `Node.js`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `Golang`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `Magento`,
+    path: OUTSOURCE,
+  },
 ];
+
+export const developers2 = [
+  {
+    title: `Java`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `Python`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `iOS`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `Android`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `Salesforce`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `AWS`,
+    path: OUTSOURCE,
+  },
+  {
+    title: `Azure`,
+    path: OUTSOURCE,
+  },
+];
+
+export const footerLists = [aboutList, [["Our Developers"], developers1, developers2]];
+
 export const copyrights = `© All rights reserved Sofomo 2020`;
---
+++ src/routes/routes.ts
@@ -0,0 +1,4 @@
+type ROUTE = string;
+
+export const HOME_PAGE: ROUTE = "/";
+export const OUTSOURCE: ROUTE = "outsource";

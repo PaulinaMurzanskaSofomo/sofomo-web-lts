5561a6a6dc01d5179ec9f6378f4462b527a669d6
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-05-12 14:23:27 +0200

    Create Image and export atoms by index

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,1 +1,6 @@
 export { Icon } from "./Icon";
+export { Typography } from "./Typography";
+export { Label } from "./Label";
+export { SectionContainer } from "./SectionContainer";
+export { StyledResponsiveContainer } from "./StyledResponsiveContainer";
+export { Image } from "./Image";

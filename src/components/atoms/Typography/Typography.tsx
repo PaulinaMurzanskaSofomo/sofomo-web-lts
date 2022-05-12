7699c6c918041326346c4e1bf9eedc88224f47e8
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-05-12 13:55:10 +0200

    Add Typography and new vars to theme

---
+++ src/components/atoms/Typography/Typography.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from "react";
+import { TypographyTypes } from "../../../types/typographyTypes";
+import { StyledTypography, StyledProps } from "./StyledTypography";
+
+interface Props extends StyledProps {
+  children: any;
+  className?: string;
+  variant?: TypographyTypes;
+  href?: string;
+}
+
+export const Typography: FC<Props> = ({
+  as = "p",
+  children,
+  color,
+  width,
+  margin,
+  hoverColor,
+  transform,
+  hoverWeight,
+  className,
+  highlightColor,
+  variant,
+  href,
+  bold,
+}) => {
+  return (
+    <StyledTypography
+      as={as}
+      href={href}
+      target="_blank"
+      className={`${variant} ${className}`}
+      width={width}
+      color={color}
+      margin={margin}
+      hoverColor={hoverColor}
+      hoverWeight={hoverWeight}
+      transform={transform}
+      highlightColor={highlightColor}
+      bold={bold ? "700" : "400"}
+    >
+      {children}
+    </StyledTypography>
+  );
+};

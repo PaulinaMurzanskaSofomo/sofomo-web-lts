54a7c7e92ffee862ac00ea35b1989b4f2458512e
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-05-12 12:51:52 +0200

    Add icons used in project make export of icons by index

---
+++ src/assets/icons/socialIconsList.tsx
@@ -0,0 +1,15 @@
+import { IconType } from "react-icons";
+import { FaLinkedinIn, FaFacebookF } from "react-icons/fa";
+
+export const socialIconsList = [
+  {
+    icon: <FaFacebookF />,
+    variant: "facebook",
+    path: "https://www.facebook.com/Sofomo-131919800556137/",
+  },
+  {
+    icon: <FaLinkedinIn />,
+    variant: "linked-in",
+    path: "https://www.linkedin.com/authwall?trk=bf&trkInfo=AQElU3Q1aZL_ygAAAX_pG7bwzTw-NI5aiqEkypuDSnAbiSgF7i_glDfRKPHuGzK1fDVbL-5PA8axCtV_gxOsn6g4DFuxW1phvXkbivNwRXwpsx8gc9A1chPDYaS86o8ODI9Ak-0=&originalReferer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fsofomo%2F",
+  },
+];

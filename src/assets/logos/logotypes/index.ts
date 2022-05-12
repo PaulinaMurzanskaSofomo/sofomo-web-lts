802b46b72f3ac428881868900a4d243ed7c09966
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-05-12 12:59:52 +0200

    Add logotypes and logos lists

---
+++ src/assets/logos/logotypes/index.ts
@@ -0,0 +1,41 @@
+import bluePipes from "./logotypes/BluePipesLogo.svg";
+import copyIn from "./logotypes/CopyIn.svg";
+import kahoots from "./logotypes/KahootsLogo.svg";
+import musclefood from "./logotypes/MuclefoodLogo.svg";
+import olimpus from "./logotypes/Olimpus.svg";
+import pepsi from "./logotypes/pepsi.png";
+import primex from "./logotypes/PrimexLogo.svg";
+import roadtrip from "./logotypes/RoadtripLogo.svg";
+import voyavoy from "./logotypes/VoyavoyLogo.svg";
+import dictionary from "./logotypes/Dictionary.svg";
+import directly from "./logotypes/Directly.svg";
+import miDrive from "./logotypes/Midrive.svg";
+import panasser from "./logotypes/Panasser.svg";
+import panasserPng from "./logotypes/Panasser2.png";
+import dictionaryColor from "./logotypes/DictionaryColor.svg";
+import januaryColor from "./logotypes/JanuaryDigitalColor.png";
+import primexColor from "./logotypes/PrimexColor.png";
+import roadtripColor from "./logotypes/RoadtripColor.png";
+import sofomo from "./logotypes/Sofomo.svg";
+
+export {
+  bluePipes,
+  copyIn,
+  kahoots,
+  musclefood,
+  olimpus,
+  pepsi,
+  primex,
+  roadtrip,
+  voyavoy,
+  dictionary,
+  directly,
+  miDrive,
+  panasser,
+  panasserPng,
+  dictionaryColor,
+  januaryColor,
+  primexColor,
+  roadtripColor,
+  sofomo,
+};

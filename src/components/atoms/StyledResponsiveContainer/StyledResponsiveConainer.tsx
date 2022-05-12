31c4404207e016ebf58107cb7fcbd93696e2e413
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-05-12 14:08:55 +0200

    Create StyledResponsiveContainer:
    This should be just styled component, it is a base to extend other
    styled components.

---
+++ src/components/atoms/StyledResponsiveContainer/StyledResponsiveConainer.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { device } from "../../../utils/devices/devices";
+
+export const StyledResponsiveContainer = styled.div`
+  margin: 0 auto;
+  /* min-width: 375px; */ //can't be here, breaks nav
+  max-width: 1094px;
+  @media ${device.tablet} {
+    max-width: 768px;
+  }
+  @media ${device.laptop} {
+    max-width: 1094px;
+  }
+`;

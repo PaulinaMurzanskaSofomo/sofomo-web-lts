c57c0aaa671343bf6aec89f01af86c2e24faeeef
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-05-14 06:39:04 +0200

    Add noopener noreffrerer attr to link

--- src/components/atoms/Icon/Icon.tsx
+++ src/components/atoms/Icon/Icon.tsx
@@ -11,7 +11,14 @@ interface Props {
 
 export const Icon: FC<Props> = ({ fontSize, path, children, variant, width }) => {
   return (
-    <StyledIcon className={variant} href={path} target="_blank" fontSize={fontSize} width={width}>
+    <StyledIcon
+      className={variant}
+      href={path}
+      target="_blank"
+      rel="noopener noreferrer"
+      fontSize={fontSize}
+      width={width}
+    >
       {children}
     </StyledIcon>
   );

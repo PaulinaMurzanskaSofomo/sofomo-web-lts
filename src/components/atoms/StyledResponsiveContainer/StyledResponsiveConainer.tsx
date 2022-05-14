3c8554fe8de6aadc665e646e437bf8c56ee3b9a6
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-05-14 07:06:34 +0200

    Remove unnesesary comment

--- src/components/atoms/StyledResponsiveContainer/StyledResponsiveConainer.tsx
+++ src/components/atoms/StyledResponsiveContainer/StyledResponsiveConainer.tsx
@@ -3,7 +3,6 @@ import { device } from "../../../utils/devices/devices";
 
 export const StyledResponsiveContainer = styled.div`
   margin: 0 auto;
-  /* min-width: 375px; */ //can't be here, breaks nav
   max-width: 1094px;
   @media ${device.tablet} {
     max-width: 768px;

4ffa85ce4a99e3d7cce9384c8b6fc4ab2596aa31
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-05-16 12:47:47 +0200

    Remove unnesesary id from logo lists

--- src/assets/logos/logoLists/logosList.ts
+++ src/assets/logos/logoLists/logosList.ts
@@ -3,26 +3,20 @@ import { musclefood, bluePipes, kahoots, roadtrip, voyavoy, primex } from "../lo
 export const partnersLogosList = [
   {
     logo: musclefood,
-    id: 1,
   },
   {
     logo: bluePipes,
-    id: 2,
   },
   {
     logo: kahoots,
-    id: 3,
   },
   {
     logo: roadtrip,
-    id: 4,
   },
   {
     logo: voyavoy,
-    id: 5,
   },
   {
     logo: primex,
-    id: 6,
   },
 ];
--- src/assets/logos/logoLists/logosOutsource.ts
+++ src/assets/logos/logoLists/logosOutsource.ts
@@ -3,22 +3,17 @@ import { olimpus, miDrive, pepsi, panasser, copyIn } from "../logotypes";
 export const logosOutsource = [
   {
     logo: olimpus,
-    id: 1,
   },
   {
     logo: miDrive,
-    id: 2,
   },
   {
     logo: pepsi,
-    id: 3,
   },
   {
     logo: panasser,
-    id: 4,
   },
   {
     logo: copyIn,
-    id: 5,
   },
 ];

43bdb2cd63e1fcf2fe11fcce453e55c81540b749
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-06-29 13:45:34 +0200

    Create routing at the App level

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,30 @@ import React from "react";
 import GlobalStyles from "./themes/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./themes/MainTheme";
-import { ContactFormContext } from "./context";
+import { ContactFormContext, NavItemStateContext, PageOverlayContext } from "./context";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { PageTemplate } from "./templates/PageTemplate";
+import { ROUTE } from "./routes";
+import { HomePage, OutsourcePage } from "./pages";
 
 function App() {
   return (
     <ContactFormContext>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <div>Sofomo web</div>
-      </ThemeProvider>
+      <NavItemStateContext>
+        <PageOverlayContext>
+          <ThemeProvider theme={theme}>
+            <BrowserRouter>
+              <GlobalStyles />
+              <PageTemplate>
+                <Routes>
+                  <Route path={ROUTE.HOME_PAGE} element={<HomePage />} />
+                  <Route path={ROUTE.OUTSOURCE} element={<OutsourcePage />} />
+                </Routes>
+              </PageTemplate>
+            </BrowserRouter>
+          </ThemeProvider>
+        </PageOverlayContext>
+      </NavItemStateContext>
     </ContactFormContext>
   );
 }
